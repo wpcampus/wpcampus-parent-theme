20b6ea13195c36a6343559a2690d35eb7188922a
Rachel Carden <Rachel.Carden@disney.com>
2018-01-27 16:17:31 -0800

    Adding WP loop, 404 page, and before and after content hooks

--- inc/theme-parts.php
+++ inc/theme-parts.php
@@ -18,3 +18,19 @@ function wpcampus_parent_print_page_title() {
 	?></h1>
 	<?php
 }
+
+/**
+ * Print the 404 page.
+ */
+function wpcampus_parent_print_404() {
+
+	do_action( 'wpc_add_before_404' );
+
+	?>
+	<p><?php _e( 'Uh-oh. This page seems to be missing. Please check to make sure the link you requested was entered correctly.', 'wpcampus' ); ?></p>
+	<p><?php printf( __( 'If you can\'t find what you\'re looking for in the menu, please %1$sreach out to us%2$s and let us know. We\'d be happy to help.', 'wpcampus' ), '<a href="/contact/">', '</a>' ); ?></p>
+	<?php
+
+	do_action( 'wpc_add_after_404' );
+
+}
--- index.php
+++ index.php
@@ -2,4 +2,19 @@
 
 get_header();
 
+do_action( 'wpc_add_before_content' );
+
+if ( ! have_posts() ) :
+	wpcampus_parent_print_404();
+else :
+	while ( have_posts() ) :
+		the_post();
+
+		the_content();
+
+	endwhile;
+endif;
+
+do_action( 'wpc_add_after_content' );
+
 get_footer();
