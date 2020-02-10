af5c39da98e821cb88ade8571a6669118d95a6c3
Rachel Cherry <bamadesigner@gmail.com>
2020-02-10 00:52:43 -0500

    Updating speaker confirmation URL to 2020

--- template-speaker-app-confirmation.php
+++ template-speaker-app-confirmation.php
@@ -7,8 +7,9 @@
 // If we came from the application, redirect out of the frame.
 if ( ! empty( $_SERVER['HTTP_REFERER'] ) && 'https://' . DOMAIN_CURRENT_SITE . '/speaker-application/' == $_SERVER['HTTP_REFERER'] ) :
 
+	// @TODO move URL to settings.
 	if ( ! empty( $_GET['redirect'] ) ) {
-		wpcampus_network()->html_redirect( 'https://2019.' . DOMAIN_CURRENT_SITE . '/call-for-speakers/confirmation/' );
+		wpcampus_network()->html_redirect( 'https://2020.' . DOMAIN_CURRENT_SITE . '/call-for-speakers/confirmation/' );
 	}
 
 	wpcampus_network()->content_only();
